2d9e876629ba741bbebaa6e0fac6d091136d64a8
Neil Burningham <neil.burningham@mediakind.com>
2019-12-05 14:11:31 +0000

    1st step

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    let people = ['Chris', 'Andrew', 'Neil'];
+
+    let items = [];
+    for (let i = 0; i < people.length; i++) {
+        items.push(<div key={i}>Hello {people[i]}</div>);
+    }
+  
+    return (
+        <div>
+            {items}
+        </div>
   );
 }
 
